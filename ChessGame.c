7e1057a0f11be550e924fb83ef2bb35ef2a77544
GuyWhoC0des <cgembarow@gmail.com>
2023-04-30 22:27:50 -0400

    added all pieces to the board

--- ChessGame.c
+++ ChessGame.c
@@ -19,25 +19,33 @@ void fillBoardGraphics(int arr[][size], int arr2[][size]){
 };
 
 //intial location for each piece
-void initializeBoardState(){
-    //B = 0->15
-    //W = 16 -> 31
+void initializeBoardState(Texture2D texture){
 
+   
 
-
-
-
-};
-
-int main(){
-
-    const int WindowWidth = 480;
-    const int WindowHeight = 480;
-    InitWindow(WindowWidth, WindowHeight, "MyGame");
-
-    //LoadImage("./Chess_blt60.png");
-    Image ChessPieceArray = LoadImage("resources/ChessPiecesArray.png");
     
+
+    Vector2 positionKB = {240,0};
+    Vector2 positionQB = {180,0};
+    Vector2 positionBB1 = {120,0};
+    Vector2 positionBB2 = {300,0};
+    Vector2 positionHB1 = {60,0};
+    Vector2 positionHB2 = {360,0};
+    Vector2 positionRB1 = {0,0};
+    Vector2 positionRB2 = {420,0};
+
+    Vector2 positionKW = {240,420};
+    Vector2 positionQW = {180,420};
+    Vector2 positionBW1 = {120,420};
+    Vector2 positionBW2 = {300,420};
+    Vector2 positionHW1 = {60,420};
+    Vector2 positionHW2 = {360,420};
+    Vector2 positionRW1 = {0,420};
+    Vector2 positionRW2 = {420,420};
+
+   
+
+
     Rectangle queen = {0, 0, 60, 60};
     Rectangle king = {60, 0, 60, 60};
     Rectangle rook = {120, 0, 60, 60};
@@ -50,13 +58,53 @@ int main(){
     Rectangle knightB = {180, 60, 60, 60};
     Rectangle bishopB = {240, 60, 60, 60};
     Rectangle pawnB = {300, 60, 60, 60};
-    Texture2D texture = LoadTextureFromImage(ChessPieceArray);
-
-
     
+    //B = 0->15
+    //W = 16 -> 31
+  //Draw BLack Pieces
+    DrawTextureRec(texture,king,positionKB,WHITE );
+    DrawTextureRec(texture,queen,positionQB,WHITE );
+    DrawTextureRec(texture,bishop,positionBB1,WHITE );
+    DrawTextureRec(texture,bishop,positionBB2,WHITE );
+    DrawTextureRec(texture,knight,positionHB1,WHITE );
+    DrawTextureRec(texture,knight,positionHB2,WHITE );
+    DrawTextureRec(texture,rook,positionRB1,WHITE );
+    DrawTextureRec(texture,rook,positionRB2,WHITE);
+    for(int i = 0; i < 8; i++){
+        DrawTextureRec(texture,pawn,(Vector2){i * 60, 60},WHITE );
+    }
+   //Draw White Pieces
+    DrawTextureRec(texture,kingB,positionKW,WHITE );
+    DrawTextureRec(texture,queenB,positionQW,WHITE );
+    DrawTextureRec(texture,bishopB,positionBW1,WHITE );
+    DrawTextureRec(texture,bishopB,positionBW2,WHITE );
+    DrawTextureRec(texture,knightB,positionHW1,WHITE );
+    DrawTextureRec(texture,knightB,positionHW2,WHITE );
+    DrawTextureRec(texture,rookB,positionRW1,WHITE);
+    DrawTextureRec(texture,rookB,positionRW2,WHITE );
+    for(int i = 0; i < 8; i++){
+        DrawTextureRec(texture,pawnB,(Vector2){i * 60, 360},WHITE);
+    }
+
+
+
+};
+
+int main(){
+
+    const int WindowWidth = 480;
+    const int WindowHeight = 480;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(WindowWidth, WindowHeight, "MyGame");
+    
+
+     //LoadImage("./Chess_blt60.png");
+    Image ChessPieceArray = LoadImage("resources/ChessPiecesArray.png");
+    Texture2D texture = LoadTextureFromImage(ChessPieceArray);
+    UnloadImage(ChessPieceArray);
   
     struct ChessBoard board;
-Vector2 position = {0,0};
+
     //intializing constants
     int x = 0;
     int y = 0;
@@ -68,43 +116,33 @@ Vector2 position = {0,0};
     //pieces on the board
     int piecesRemaning = 32;
 
-    
-
-
-   
     SetTargetFPS(60);
 
     fillBoardGraphics(board.BoardGraphics, board.BoardCells);
     //initializeBoardState();
 
-   
-    while(!WindowShouldClose())
-    {
+    while(!WindowShouldClose()){
         BeginDrawing();
 
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
                 if(board.BoardGraphics[i][j] == 0){
-                    r = 255;
-                    g = 255;
-                    b = 255;
+                    r = 232;
+                    g = 220;
+                    b = 202;
                     a = 255;
                 }
                 else {
-                    r = 0;
-                    g = 255;
-                    b = 0;
+                    r = 34;
+                    g = 139;
+                    b = 34;
                     a = 255;
                 }
-                DrawRectangle(x + i*60, y + j*60, 60, 60, (Color){r,g,b,a});
-                
+                DrawRectangle(x + i*60, y + j*60, 60, 60, (Color){r,g,b,a});  
             }
-            
         }
-       DrawTextureRec(texture,king,position,BLACK );
-     
-       EndDrawing(); 
+      initializeBoardState(texture);
+        EndDrawing(); 
     }
-
     CloseWindow();
 }
