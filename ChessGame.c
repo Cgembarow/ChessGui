4a017ecae3c7b410bae577a3549521b169a3ecaa
GuyWhoC0des <cgembarow@gmail.com>
2023-06-02 23:41:16 -0400

    rewrote code to better handle boardstate and setup the ability to move pieces

--- ChessGame.c
+++ ChessGame.c
@@ -1,191 +1,192 @@
-#include<stdio.h>
 #include <raylib.h>
+#include <string.h>
+
 #define size 8
 
-struct ChessBoard{
-    int BoardCells[size][size];
-    int BoardGraphics[size][size];
-    int BoardState[size][size];
+struct ChessBoard {
+    int boardGraphics[size][size];
+    char* boardState[size][size];
 };
 
-struct Piece{
 
-};
 
-//determine if cell is green or white
-void fillBoardGraphics(int arr[][size], int arr2[][size]){
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            if((j-i) %2 == 0)arr[i][j] = 0;
-            else arr[i][j] = 1; 
+void fillBoardGraphics(int arr[][size]) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if ((j - i) % 2 == 0)
+                arr[i][j] = 0;
+            else
+                arr[i][j] = 1;
         }
     }
-};
+}
 
-//intial location for each piece
-void initializeBoardState(Texture2D texture){
+void initializeBoardState(char* boardState[size][size]) {
+    char* startingPositions[size][size] = {
+        {"BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"},
+        {"BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"},
+        {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+        {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+        {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+        {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+        {"WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"},
+        {"WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"}
+    };
 
-    Vector2 positionKB = {240,0};
-    Vector2 positionQB = {180,0};
-    Vector2 positionBB1 = {120,0};
-    Vector2 positionBB2 = {300,0};
-    Vector2 positionHB1 = {60,0};
-    Vector2 positionHB2 = {360,0};
-    Vector2 positionRB1 = {0,0};
-    Vector2 positionRB2 = {420,0};
+    memcpy(boardState, startingPositions, sizeof(startingPositions));
+}
 
-    Vector2 positionKW = {240,420};
-    Vector2 positionQW = {180,420};
-    Vector2 positionBW1 = {120,420};
-    Vector2 positionBW2 = {300,420};
-    Vector2 positionHW1 = {60,420};
-    Vector2 positionHW2 = {360,420};
-    Vector2 positionRW1 = {0,420};
-    Vector2 positionRW2 = {420,420};
+void drawBoardState(char* boardState[size][size], Texture2D texture) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (boardState[i][j] != NULL) {
+                int rowOffset = 0;
+                int columnOffset = 0;
 
-   
-    Rectangle queen = {0, 0, 60, 60};
-    Rectangle king = {60, 0, 60, 60};
-    Rectangle rook = {120, 0, 60, 60};
-    Rectangle knight = {180, 0, 60, 60};
-    Rectangle bishop = {240, 0, 60, 60};
-    Rectangle pawn = {300, 0, 60, 60};
+                // Determine the row and column offsets based on the piece color
+                if (boardState[i][j][0] == 'W') {
+                    rowOffset = 60;
+                }
 
-    Rectangle queenB = {0, 60, 60, 60};
-    Rectangle kingB = {60, 60, 60, 60};
-    Rectangle rookB = {120, 60, 60, 60};
-    Rectangle knightB = {180, 60, 60, 60};
-    Rectangle bishopB = {240, 60, 60, 60};
-    Rectangle pawnB = {300, 60, 60, 60};
-    
-    //B = 0->15
-    //W = 16 -> 31
-  //Draw BLack Pieces
-    DrawTextureRec(texture,king,positionKB,WHITE );
-    DrawTextureRec(texture,queen,positionQB,WHITE );
-    DrawTextureRec(texture,bishop,positionBB1,WHITE );
-    DrawTextureRec(texture,bishop,positionBB2,WHITE );
-    DrawTextureRec(texture,knight,positionHB1,WHITE );
-    DrawTextureRec(texture,knight,positionHB2,WHITE );
-    DrawTextureRec(texture,rook,positionRB1,WHITE );
-    DrawTextureRec(texture,rook,positionRB2,WHITE);
-    for(int i = 0; i < 8; i++){
-        DrawTextureRec(texture,pawn,(Vector2){i * 60, 60},WHITE );
+                // Determine the column offset based on the piece type
+                switch (boardState[i][j][1]) {
+                    case 'Q':
+                        columnOffset = 0;
+                        break;
+                    case 'K':
+                        columnOffset = 60;
+                        break;
+                    case 'R':
+                        columnOffset = 120;
+                        break;
+                    case 'N':
+                        columnOffset = 180;
+                        break;
+                    case 'B':
+                        columnOffset = 240;
+                        break;
+                    case 'P':
+                        columnOffset = 300;
+                        break;
+                    default:
+                        // Invalid piece type, skip drawing
+                        continue;
+                }
+
+                // Draw the piece from the texture
+                Rectangle sourceRec = { columnOffset, rowOffset, 60, 60 };
+                Vector2 position = { j * 60, i * 60 };
+                DrawTextureRec(texture, sourceRec, position, WHITE);
+            }
+        }
     }
+}
 
-   //Draw White Pieces
-    DrawTextureRec(texture,kingB,positionKW,WHITE );
-    DrawTextureRec(texture,queenB,positionQW,WHITE );
-    DrawTextureRec(texture,bishopB,positionBW1,WHITE );
-    DrawTextureRec(texture,bishopB,positionBW2,WHITE );
-    DrawTextureRec(texture,knightB,positionHW1,WHITE );
-    DrawTextureRec(texture,knightB,positionHW2,WHITE );
-    DrawTextureRec(texture,rookB,positionRW1,WHITE);
-    DrawTextureRec(texture,rookB,positionRW2,WHITE );
-    for(int i = 0; i < 8; i++){
-        DrawTextureRec(texture,pawnB,(Vector2){i * 60, 360},WHITE);
+void handleMouseInput(struct ChessBoard* board, int* redx, int* redy) {
+    Vector2 mouseposition = GetMousePosition();
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        bool clickedOnTile = false;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (mouseposition.x >= i * 60 && mouseposition.x < (i + 1) * 60 &&
+                    mouseposition.y >= j * 60 && mouseposition.y < (j + 1) * 60) {
+
+                    // if the rectangle is not shown, show it on click
+                    if (*redx == i && *redy == j) {
+                        *redx = -1;
+                        *redy = -1;
+                        continue;
+                    }
+                    *redx = i;
+                    *redy = j;
+                }
+            }
+        }
     }
+}
 
+void drawBoard(struct ChessBoard* board, int redx, int redy) {
+    int r, g, b, a;
 
-
-};
-
-
-void movePiece(){
-
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            // if is red then stay red
+            if (i == redx && j == redy) {
+                DrawRectangle(i * 60, j * 60, 60, 60, RED);
+                //if piece on square then piece.pos = mouseposition
+                continue;
+            }
+            if (board->boardGraphics[i][j] == 0) {
+                r = 232;
+                g = 220;
+                b = 202;
+                a = 255;
+            } else {
+                r = 34;
+                g = 139;
+                b = 34;
+                a = 255;
+            }
+            DrawRectangle(i * 60, j * 60, 60, 60, (Color){ r, g, b, a });
+        }
+    }
 }
 
 
+    
+void swapElements(char* boardState[size][size], int x1, int y1, int x2, int y2) {
+    char* temp = boardState[x1][y1];
+    boardState[x1][y1] = boardState[x2][y2];
+    boardState[x2][y2] = temp;
+}
 
-int main(){
+void updateBoardState(char* boardState[size][size]){
+    /*find first mouse click
+    if mouse click on a piece and not null then piece location = mouse location
+    find second mouse click
+    check if piece is on the square
+    if true replace
+    if false piece location = square clicked on
+    */
+   
+}
 
+int main() {
     int redx = -1;
     int redy = -1;
     const int WindowWidth = 480;
     const int WindowHeight = 480;
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(WindowWidth, WindowHeight, "SUPER AWESOME JOE BIDEN CHESS");
-    
 
-     //LoadImage("./Chess_blt60.png");
     Image ChessPieceArray = LoadImage("resources/ChessPiecesArray.png");
     Texture2D texture = LoadTextureFromImage(ChessPieceArray);
     UnloadImage(ChessPieceArray);
-  
+
     struct ChessBoard board;
 
-    //intializing constants
+    // intializing constants
     int x = 0;
     int y = 0;
-    int r = 0;
-    int g = 0;
-    int b = 0;
-    int a = 0;
-
-    //pieces on the board
-    int piecesRemaning = 32;
 
     SetTargetFPS(60);
 
-    fillBoardGraphics(board.BoardGraphics, board.BoardCells);
-    //initializeBoardState();
+    fillBoardGraphics(board.boardGraphics);
+    initializeBoardState(board.boardState);
+    //swapElements(board.boardState, 0, 0, 1, 1);
 
-    while(!WindowShouldClose()){
+    while (!WindowShouldClose()) {
+
+        //updateBoard(board.boardState);
         BeginDrawing();
 
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                // if is red then stay red 
-                if(i == redx && j == redy){
-                    DrawRectangle(i*60, j*60, 60, 60, RED);
-                    continue;
-                }
-                if(board.BoardGraphics[i][j] == 0){
-                    r = 232;
-                    g = 220;
-                    b = 202;
-                    a = 255;
-                }
-                else {
-                    r = 34;
-                    g = 139;
-                    b = 34;
-                    a = 255;
-                }
-                DrawRectangle(x + i*60, y + j*60, 60, 60, (Color){r,g,b,a});  
-            }
-        }
-        bool rectangleShown = false; // initialize the state to not shown
+        handleMouseInput(&board, &redx, &redy);
+        drawBoard(&board, redx, redy);
+        drawBoardState(board.boardState, texture);
 
-        Vector2 mouseposition = GetMousePosition();
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            bool clickedOnTile = false;
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if (mouseposition.x >= i*60 && mouseposition.x < (i+1)*60 &&
-                        mouseposition.y >= j*60 && mouseposition.y < (j+1)*60) {
-                       
-                         
-                            
-                            // if the rectangle is not shown, show it on click
-                            if(redx == i && redy == j){
-                                    redx = -1; 
-                                    redy = -1;
-                                    continue;
-                                }  
-                            redx = i;
-                            redy = j;
-                    }
-                }
-            }
-        }
-      initializeBoardState(texture);
-     
-
-        EndDrawing(); 
+        EndDrawing();
     }
+
     CloseWindow();
+    return 0;
 }
-
-//cc ChessGame.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-
